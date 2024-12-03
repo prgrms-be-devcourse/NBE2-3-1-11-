a29ec7c7f3754b49c95ab7ce6751a05a2e4b2ef0
박현욱 <nic991002@gmail.com>
2024-12-03 12:45:43 +0900

    Initial Commit

---
+++ order01/src/main/java/com/example/order/Order01Application.java
@@ -0,0 +1,13 @@
+package com.example.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Order01Application.class, args);
+	}
+
+}
