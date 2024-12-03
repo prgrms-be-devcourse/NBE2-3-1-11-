67482da5f64b0f5bd148b98a90b67435538a7ed2
oasis23ckr@gmail.com <oasis23ckr@gmail.com>
2024-12-03 16:45:27 +0900

    testCommit 2

--- order01/src/main/java/com/example/order/controller/OrderController.java
+++ order01/src/main/java/com/example/order/controller/OrderController.java
@@ -1,8 +1,13 @@
 package com.example.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
 
+    @RequestMapping("/order")
+    public void test() {
+        System.out.println("test");
+    }
 }
