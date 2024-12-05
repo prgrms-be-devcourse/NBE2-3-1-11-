9ba3b95712faa98bbe18f9f7f74dfa2ee6295b24
Gabin Nam <cabin4nam@naver.com>
2024-12-05 12:32:14 +0900

    [fix] check date

--- order01/src/main/java/com/example/order/mapper/OrderMapper.java
+++ order01/src/main/java/com/example/order/mapper/OrderMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface OrderMapper {
     // 주문 삭제
-    @Delete("DELETE FROM orders WHERE orderId = #{orderId} AND date = CURDATE()")
+    @Delete("DELETE FROM orders WHERE orderId = #{orderId} AND DATE(date) = CURDATE()")
     int deleteOrder(long orderId);
 }
